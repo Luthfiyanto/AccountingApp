7b3347d1ee79d64c6eafb73976219c8770c32920
Luthfiyanto <luthfiyantooo@students.unnes.ac.id>
2024-12-06 17:18:46 +0700

    feat: update transaction

--- models/transactions.py
+++ models/transactions.py
@@ -53,35 +53,71 @@ def get_all_transactions():
     conn.close()
     return transactions
 
-def get_transactions(account_id):
+def get_transactions():
     conn = create_connection()
     cursor = conn.cursor()
-    cursor.execute('''SELECT * FROM transactions WHERE account_id = ?''', (account_id,))
+    cursor.execute('''SELECT * FROM transactions''')
     transactions = cursor.fetchall()
+    conn.commit()
     conn.close()
     return transactions
-
-def get_total_balance(account_id):
+    
+def get_transaction(transaction_id):
     conn = create_connection()
     cursor = conn.cursor()
-    cursor.execute('''SELECT SUM(amount) FROM transactions WHERE account_id = ?''', (account_id,))
-    total_balance = cursor.fetchone()[0]
+    cursor.execute('''
+        SELECT 
+            td.id AS id,
+            td.transaction_id AS transaction_id,
+            t.date AS date,
+            t.description AS description,
+            td.account AS account,
+            CASE WHEN td.account_type = 'debit' THEN td.amount ELSE 0 END AS debit,
+            CASE WHEN td.account_type = 'credit' THEN td.amount ELSE 0 END AS credit
+        FROM
+            transactions_detail td
+        JOIN
+            transactions t ON td.transaction_id = t.id
+        LEFT JOIN
+            accounts a ON td.account = a.account_name
+        WHERE
+            t.id = ?
+    ''', (transaction_id,))
+    transaction = cursor.fetchall()
     conn.close()
-    return total_balance
-
-def get_all_balances():
+    return transaction
+    
+def get_transaction_detail(transaction_id):
     conn = create_connection()
     cursor = conn.cursor()
-    cursor.execute('''SELECT account_id, SUM(amount) FROM transactions GROUP BY account_id''')
-    balances = cursor.fetchall()
+    cursor.execute('''
+        SELECT * FROM transactions_detail WHERE transaction_id = ?
+    ''', (transaction_id,))
+    transaction = cursor.fetchall()
     conn.close()
-    return balances
+    return transaction
 
-def update_transaction(transaction_id, amount, date, description):
+def update_transaction(transaction_id, date, description, debit_accounts, credit_accounts):
     conn = create_connection()
     cursor = conn.cursor()
-    cursor.execute('''UPDATE transactions SET amount = ?, date = ?, description = ? 
-                      WHERE id = ?''', (amount, date, description, transaction_id))
+    cursor.execute('''
+            UPDATE transactions 
+            SET date = ?, description = ? 
+            WHERE id = ?''', (date, description, transaction_id))
+    
+    cursor.execute('''DELETE FROM transactions_detail WHERE transaction_id = ?''', (transaction_id,))
+
+    for account, amount in debit_accounts:
+        cursor.execute(
+            "INSERT INTO transactions_detail (transaction_id, account_type, account, amount) VALUES (?,?,?,?)",
+            (transaction_id, 'debit', account, amount)
+        )
+    
+    for account, amount in credit_accounts:
+        cursor.execute(
+            "INSERT INTO transactions_detail (transaction_id, account_type, account, amount) VALUES (?,?,?,?)",
+            (transaction_id, 'credit', account, amount)
+        )
     conn.commit()
     conn.close()
 
--- page/transactions.py
+++ page/transactions.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import datetime
 import pandas as pd
-from models.transactions import get_all_transactions, save_transactions
+from models.transactions import get_all_transactions, save_transactions, get_transactions, get_transaction_detail, update_transaction
 
 # inisialisasi state
 if "transactions" not in st.session_state:
@@ -12,53 +12,96 @@ st.session_state["transactions"] = get_all_transactions()
 # def TransactionPage():
 st.subheader("Jurnal Umum")
 
-        # Form untuk menambahkan transaksi
-col1, col2 = st.columns(2)
-with col1:
-            date = st.date_input("Tanggal", datetime.date.today())
-            description = st.selectbox("Deskripsi", [
-                "Penjualan barang dagang",
-                "Pembelian barang dagang",
-                "Pembayaran gaji karyawan",
-                "Pembayaran listrik, air & sampah"
-            ])
-with col2:
-            # Pilih lebih dari satu akun untuk debit dan kredit
-            debit_accounts = st.multiselect("Akun Debit", options=(st.session_state["accounts"]))
-            debit_amounts = [st.number_input(f"Jumlah Debit untuk {account}", min_value=0) for account in debit_accounts]
+tab_add, tab_edit = st.tabs(["Add Transaction", "Edit Transaction"])
+
+with tab_add:
+    # Form untuk menambahkan transaksi
+    col1, col2 = st.columns(2)
+    with col1:
+                date = st.date_input("Tanggal", datetime.date.today())
+                description = st.selectbox("Deskripsi", [
+                    "Penjualan barang dagang",
+                    "Pembelian barang dagang",
+                    "Pembayaran gaji karyawan",
+                    "Pembayaran listrik, air & sampah"
+                ])
+    with col2:
+                # Pilih lebih dari satu akun untuk debit dan kredit
+                debit_accounts = st.multiselect("Akun Debit", options=(st.session_state["accounts"]))
+                debit_amounts = [st.number_input(f"Jumlah Debit untuk {account}", min_value=0) for account in debit_accounts]
+            
+                credit_accounts = st.multiselect("Akun Kredit", options=(st.session_state["accounts"]))
+                credit_amounts = [st.number_input(f"Jumlah Kredit untuk {account}", min_value=0) for account in credit_accounts]
+
+    if st.button("Simpan Transaksi"):
+                total_debit = sum(debit_amounts) 
+                total_credit = sum(credit_amounts)
+
+                debit_data = list(zip(debit_accounts,debit_amounts))
+                credit_data = list(zip(credit_accounts,credit_amounts))
+
+                if total_debit == total_credit:
+                # Simpan transaksi ke dalam sesi
+                    save_transactions(date, description, debit_data, credit_data)
+                    st.success("Transaksi berhasil disimpan.")
+                    st.session_state["transactions"] = get_all_transactions()
+    else:
+                st.error("Total Jumlah Debit dan Kredit harus sama.")
+
+            # Tabel Jurnal umum
+    st.write("### Daftar Transaksi")
+    transaction_df = pd.DataFrame(st.session_state["transactions"], columns=["ID","ID Transaksi", "Tanggal", "Deskripsi", "Akun", "Debit","Kredit"])
+
+    total_debit = transaction_df["Debit"].sum()
+    total_credit = transaction_df["Kredit"].sum()
+    total_balance = total_debit - total_credit
+
+    summary = {
+        "Total Debit": total_debit,
+        "Total Kredit": total_credit,
+        "Total Balance": total_balance
+    }
+
+    st.table(transaction_df)
+
+    st.table(summary)
+
+# Edit transaction
+with tab_edit:
+    trans_data = pd.DataFrame(get_transactions(), columns=["ID Transaksi", "Tanggal", "Deskripsi"])
+    transaction_id = st.selectbox("Pilih Transaksi", options=(trans_data["ID Transaksi"]))
+    
+    if transaction_id:
+        recent_t_detail = get_transaction_detail(transaction_id)
+
+        newDate = st.date_input("Tanggal Baru", value=datetime.datetime.strptime(trans_data.loc[trans_data["ID Transaksi"] == transaction_id, "Tanggal"].values[0], "%Y-%m-%d"))
+        newDescription = st.selectbox("Deskripsi Baru", [
+            "Penjualan barang dagang",
+            "Pembelian barang dagang",
+            "Pembayaran gaji karyawan",
+            "Pembayaran listrik, air & sampah"
+        ])
         
-            credit_accounts = st.multiselect("Akun Kredit", options=(st.session_state["accounts"]))
-            credit_amounts = [st.number_input(f"Jumlah Kredit untuk {account}", min_value=0) for account in credit_accounts]
+        debit_entries = [(d[3], d[4]) for d in recent_t_detail if d[2] == "debit"]
+        credit_entries = [(d[3], d[4]) for d in recent_t_detail if d[2] == "credit"]
 
-if st.button("Simpan Transaksi"):
-            total_debit = sum(debit_amounts) 
-            total_credit = sum(credit_amounts)
+        new_debit_accounts = st.multiselect("Akun Debit",options=(st.session_state["accounts"]), default=([d[0] for d in debit_entries]) )
+        new_debit_amounts = [st.number_input(f"Jumlah Debit untuk {account}", min_value=0, value=amount) for account, amount in debit_entries]
 
-            debit_data = list(zip(debit_accounts,debit_amounts))
-            credit_data = list(zip(credit_accounts,credit_amounts))
+        new_credit_accounts = st.multiselect("Akun Kredit", options=(st.session_state["accounts"]), default=([d[0] for d in credit_entries]))
+        new_credit_amounts = [st.number_input(f"Jumlah Kredit untuk {account}", min_value=0, value=amount) for account, amount in credit_entries]
+
+        if st.button("Simpan Perubahan"):
+            total_debit = sum(new_debit_amounts) 
+            total_credit = sum(new_credit_amounts)
+
+            debit_data = list(zip(new_debit_accounts,new_debit_amounts))
+            credit_data = list(zip(new_credit_accounts,new_credit_amounts))
 
             if total_debit == total_credit:
-            # Simpan transaksi ke dalam sesi
-                save_transactions(date, description, debit_data, credit_data)
-                st.success("Transaksi berhasil disimpan.")
+                update_transaction(transaction_id, newDate, newDescription, debit_data, credit_data)
+                st.success("Transaksi berhasil diubah.")
                 st.session_state["transactions"] = get_all_transactions()
-else:
-            st.error("Total Jumlah Debit dan Kredit harus sama.")
-
-        # Tabel Jurnal umum
-st.write("### Daftar Transaksi")
-transaction_df = pd.DataFrame(st.session_state["transactions"], columns=["ID","ID Transaksi", "Tanggal", "Deskripsi", "Akun", "Debit","Kredit"])
-
-total_debit = transaction_df["Debit"].sum()
-total_credit = transaction_df["Kredit"].sum()
-total_balance = total_debit - total_credit
-
-summary = {
-    "Total Debit": total_debit,
-    "Total Kredit": total_credit,
-    "Total Balance": total_balance
-}
-
-st.table(transaction_df)
-
-st.table(summary)
+            else:
+                st.error("Total Jumlah Debit dan Kredit harus sama.")
+    
